a6c9b972b8b88e4b320eb371bc1b4747091630d5
Vietvmq <vietvmq@smartgift.vn>
2023-09-28 16:35:48 +0700

    chat

---
+++ src/components/ChatPage.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h2>Chat</h2>
+    <!-- Hiển thị danh sách tin nhắn -->
+    <div class="message-list">
+      <div v-for="(message, index) in messages" :key="index">
+        {{ message.sender }}: {{ message.text }}
+      </div>
+    </div>
+
+    <!-- Hộp nhập tin nhắn -->
+    <div class="message-input">
+      <input type="text" v-model="newMessage" @keyup.enter="sendMessage" placeholder="Nhập tin nhắn...">
+      <button @click="sendMessage">Gửi</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatPage',
+  data() {
+    return {
+      newMessage: '',
+      messages: [], // Danh sách tin nhắn
+    };
+  },
+  methods: {
+    sendMessage() {
+      // Gửi tin nhắn và thêm tin nhắn mới vào danh sách
+      if (this.newMessage.trim() === '') return;
+
+      this.messages.push({
+        text: this.newMessage,
+        sender: 'Bạn', // Tên của người gửi
+      });
+
+      this.newMessage = ''; // Xóa nội dung hộp nhập tin nhắn
+    },
+  },
+};
+</script>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h2>Đăng nhập</h2>
+    <form @submit.prevent="login" v-if="!loggedIn">
+      <div>
+        <label for="username">Tài khoản:</label>
+        <input type="text" id="username" v-model="username" required>
+      </div>
+      <div>
+        <label for="password">Mật khẩu:</label>
+        <input type="password" id="password" v-model="password" required>
+      </div>
+      <div>
+        <button type="submit">Đăng nhập</button>
+      </div>
+    </form>
+    <ChatPage v-else />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import ChatPage from "@/components/ChatPage.vue";
+
+const username = ref("");
+const password = ref("");
+const loggedIn = ref(false);
+
+const login = () => {
+  if (username.value === "viet" && password.value === "123") {
+    loggedIn.value = true;
+  }
+};
+</script>
